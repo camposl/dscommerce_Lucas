4bf0900812351993e1794bce89a21cca49e002bf
camposl <lucascampos25@gmail.com>
2025-01-24 14:01:25 -0300

    Exception handling

--- src/main/java/com/estudos/dscommerce/services/ProductService.java
+++ src/main/java/com/estudos/dscommerce/services/ProductService.java
@@ -3,12 +3,19 @@ package com.estudos.dscommerce.services;
 import com.estudos.dscommerce.dto.ProductDTO;
 import com.estudos.dscommerce.entities.Product;
 import com.estudos.dscommerce.repositories.ProductRepository;
+import com.estudos.dscommerce.services.exceptions.DatabaseException;
+import com.estudos.dscommerce.services.exceptions.ResourceNotFoundException;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +25,7 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    /* OPLÇAO COMPLETA
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
@@ -25,12 +33,14 @@ public class ProductService {
         ProductDTO dto = new ProductDTO(product);
         return dto;
 
-        /* OPÇÃO RESUMIDA
+     */
+
+
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
-        */
+
     }
 
     @Transactional(readOnly = true)
@@ -50,16 +60,30 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
-        Product entity = repository.getReferenceById(id);
-        copyDtoToEntity(dto, entity);
-        entity = repository.save(entity);
+        try{
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
 
-        return new ProductDTO(entity);
+            return new ProductDTO(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id){
-        repository.deleteById(id);
+        if(!repository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try{
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Falha de integridade referencial");
+        }
 
 }
 
