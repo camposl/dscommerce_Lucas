e2233f6c99b11240ced7aeded8dccd56975cb43e
camposl <lucascampos25@gmail.com>
2025-01-29 15:52:36 -0300

    Bean Validation

---
+++ src/main/java/com/estudos/dscommerce/dto/ValidationError.java
@@ -0,0 +1,23 @@
+package com.estudos.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+
+    private List<FieldMenssage> errors = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMenssage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message){
+        errors.add(new FieldMenssage(fieldName, message));
+    }
+
+}
